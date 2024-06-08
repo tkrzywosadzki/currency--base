aef00ec36e6bd66243f6d049e633113cb9ee5f9d
Tomasz Krzywosadzki <t.krzywosadzki@gmail.com>
2024-06-08 17:11:56 +0200

    add CurrencyForm.test.js

---
+++ src/components/CurrencyForm/CurrencyForm.test.js
@@ -0,0 +1,51 @@
+import { render, screen } from '@testing-library/react';
+import CurrencyForm from './CurrencyForm';
+import userEvent from '@testing-library/user-event';
+import { cleanup } from '@testing-library/react';
+
+
+describe('Component CurrencyForm', () => {
+    it('should render without crashing', () => {
+        render(<CurrencyForm action ={() => {}} />)
+    });
+    it('should run action callback with proper data on form submit', () => {
+        const testCases = [
+            { amount: '100', from: 'PLN', to: 'USD' },
+            { amount: '123', from: 'USD', to: 'PLN' },
+            { amount: '111', from: 'PLN', to: 'USD' },
+            { amount: '999', from: 'USD', to: 'PLN' },
+            { amount: '29', from: 'PLN', to: 'USD' },
+            { amount: '20', from: 'USD', to: 'PLN' },
+            { amount: '1', from: 'PLN', to: 'USD' },
+        ]
+
+        for(const testObj of testCases) {
+            const action = jest.fn();
+        
+            // render component
+            render(<CurrencyForm action={action} />);
+        
+            // find “convert” button
+            const submitButton = screen.getByText('Convert');
+        
+            // find fields elems
+            const amountField = screen.getByTestId('amount');
+            const fromField = screen.getByTestId('from-select');
+            const toField = screen.getByTestId('to-select');
+        
+            // set test values to fields
+            userEvent.type(amountField, testObj.amount);
+            userEvent.selectOptions(fromField, testObj.from);
+            userEvent.selectOptions(toField, testObj.to);
+        
+            // simulate user click on "convert" button
+            userEvent.click(submitButton);
+        
+            // check if action callback was called once and with proper argument
+            expect(action).toHaveBeenCalledTimes(1);
+            expect(action).toHaveBeenCalledWith({ amount: Number(testObj.amount), from: testObj.from, to: testObj.to });
+
+            cleanup();
+        }});
+
+});
